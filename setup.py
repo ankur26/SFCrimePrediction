f1a9a3ab509bb803efdc29c362d03c6266e6fb17
Ankur Bhatkalkar <ankurbhatkalkar@users.noreply.github.com>
2020-02-11 21:32:15 -0500

    Updated Cookiecutter structure and notebooks

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predicting type of Crime Using Open Data from San Francisco',
+    author='Ankur Bhatkalkar',
+    license='MIT',
+)
