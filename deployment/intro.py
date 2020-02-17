2c84cfdab8aa64ddf9d85fcfff3b9f0778d6a408
Ankur Bhatkalkar <ankurbhatkalkar@users.noreply.github.com>
2020-02-17 15:57:48 -0500

    Update intro.py

--- deployment/intro.py
+++ deployment/intro.py
@@ -2,16 +2,32 @@ import streamlit as st
 import numpy as np 
 import pandas as pd
 from catboost import *
-
-print("Imports done")
-@st.cache
+import pickle
+#print("Imports done")
+@st.cache(persist=True)
 def load_model():
     model = CatBoost()
     model.load_model(fname = "C:\\Users\\Ankur Bhatkalkar\\Documents\\GitHub\\SFCrimePrediction\\models\\final_model")
     return model
+@st.cache(persist=True)
+def load_location():
+    data = pd.read_csv("C:\\Users\\Ankur Bhatkalkar\\Documents\\GitHub\\SFCrimePrediction\\deployment\\location.csv",keep_default_na=False)
+    return data.drop("Unnamed: 0",axis=1)
+
+@st.cache(persist=True)
+def load_max_min_location():
+    file = open("C:\\Users\\Ankur Bhatkalkar\\Documents\\GitHub\\SFCrimePrediction\\deployment\\location_dict","rb")
+    return pickle.load(file)
 
 st.title("Know what crimes happen in your area!!")
-st.subheader("Using San Francisco Open Crime Data this a model designed to predict which crimes\n"+
-        "are most likely to happen depending on factors used in the dataset")
+st.header("Using San Francisco Open Crime Data this a model designed to predict which crimes are most likely to happen depending on factors used in the dataset")
+model = load_model()
+locations = load_location()
+location_dict= load_max_min_location()
+st.subheader("Enter the parameters")
+st.slider(label="Longitude",min_value = float(location_dict["min_longitude"][0]),
+          value=float(location_dict["min_longitude"][0]),max_value = float(location_dict["max_longitude"][0]),step=0.0001,key="Longitude")
 
+st.slider(label="Latitude",min_value = float(location_dict["min_latitude"][0]),
+          value=float(location_dict["min_latitude"][0]),max_value = float(location_dict["max_latitude"][0]),step=0.0001,key="Latitude")
 
